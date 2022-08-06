c488fffb6c1fb9040b4c4c63954efb573c916fc9
KarenKar11 <margaryank693@gmail.com>
2022-08-05 23:30:57 -0800

    new

---
+++ js/main.js
@@ -0,0 +1,45 @@
+const fadeOut = () => {
+    const loaderWrapper =
+        document.querySelector(".wrapper");
+    loaderWrapper.classList.add("fade");
+}
+
+window.addEventListener("load", fadeOut);
+
+// scroll top start
+
+var mybutton = document.getElementById("myBtn");
+
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+
+
+// scroll top end
+
+// Push Content start
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+  }
+
+// Push Content end
+
