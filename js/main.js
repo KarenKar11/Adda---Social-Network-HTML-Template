e6c8b509594fe5d588136304d175527e19820510
KarenKar11 <margaryank693@gmail.com>
2022-08-07 05:19:00 -0800

    html/css/js

--- js/main.js
+++ js/main.js
@@ -33,12 +33,14 @@ function topFunction() {
 
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("main").style.marginLeft = "250px";   
+    document.getElementById("main").style.opacity = "0";   
 }
 
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
+    document.getElementById("main").style.opacity = "1";   
 }
 
 // Push Content end
